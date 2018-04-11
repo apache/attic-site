2bf2ad35030c649e4c7ab100c1977bfdf4409bcf
Henk P. Penning <henkp@apache.org>
2018-04-11 15:55:27 +0000

    init attic_filter.lua
    git-svn-id: https://svn.apache.org/repos/asf/attic/site@1828911 13f79535-47bb-0310-9956-ffa450edef68

--- docs/scripts/attic_filter.lua
+++ docs/scripts/attic_filter.lua
@@ -1,18 +1,19 @@
 function output_filter(r)
     -- We only filter text/html types
     if not r.content_type:match(".*text/html.*") then return end
- 
+
     -- add header:
     local host = r.hostname:match("^([^.]+)") -- get TLP part of hostname
     coroutine.yield(([[
         <div style='width: 100%; height: 32px; color: #FFF; background: #C00;'>
-            This project has retired! See %s's
-            <a href='https://attic.apache.org/projects/%s.html'>attic</a> page.
+            This project has retired! See
+            <a href='https://attic.apache.org/%s.html'>attic.apache.org/%s.html</a>.
         </div>
         ]]):format(host, host))
- 
+
     -- spit out the actual page
     while bucket do
         coroutine.yield(bucket)
     end
+
 end
