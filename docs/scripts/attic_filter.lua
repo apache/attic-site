7cc3f4998721e5087e361dca930a74f2cba1b0d4
Sebb <sebb@apache.org>
2018-04-12 23:47:37 +0000

    Surely this was working?
    git-svn-id: https://svn.apache.org/repos/asf/attic/site@1829021 13f79535-47bb-0310-9956-ffa450edef68

--- docs/scripts/attic_filter.lua
+++ docs/scripts/attic_filter.lua
@@ -1,16 +1,3 @@
---[[
-  This is an output filter for HTML files
-  It adds a banner for projects that are flagged as in the attic.
-  
-  It is invoked by the tlp vhosts if the following directory exists:
-  /var/www/attic.apache.org/flagged/%{HTTP_HOST}
-
-  See the tlp vhost definitions in puppet-infra data/nodes/(themis|tlp-eu-west).apache.org.yaml
-  
-  Note: This filter was introduced in April 2018, so not all projects in the Attic use this filter. 
-  Previously the project websites themselves were changed.
-]]--
-
 function output_filter(r)
     -- We only filter text/html types
     if not r.content_type:match(".*text/html.*") then return end
