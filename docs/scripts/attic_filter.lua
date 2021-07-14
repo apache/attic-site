9d64cdda5a992a18e9a4ab0b0d19c6375f781c08
buildbot <buildbot@unknown>
2021-07-14 12:22:01 +0000

    Automatic Site Publish by Buildbot
    git-svn-id: https://svn.apache.org/repos/asf/attic/site@1891538 13f79535-47bb-0310-9956-ffa450edef68

--- docs/scripts/attic_filter.lua
+++ docs/scripts/attic_filter.lua
@@ -18,16 +18,16 @@ function output_filter(r)
     -- add header:
     -- get TLP part of hostname
     local host = r.hostname:match("^([^.]+)")
-    -- Prettify (TODO: look up real name)
-    local name = host:gsub("^%l", string.upper)
+
     local sty1 = 'font-size:x-large;padding:15px;color:white;background:red;' ;
     local sty2 = 'color:white;text-decoration:underline' ;
     local div = ([[
       <div style='%s'>
-        Project <i>%s</i> has retired. For details please refer to its
+        This project has retired. For details please refer to its
         <a style='%s' href="https://attic.apache.org/projects/%s.html">
         Attic page</a>.
-      </div>]]):format(sty1, name, sty2, host)
+      </div>]]):format(sty1, sty2, host)
+
     -- special processing needed for some hosts
     if host == 'predictionio' or host == 'eagle' or host == 'metamodel'
     then
